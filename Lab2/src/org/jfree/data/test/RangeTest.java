132649dbfe5464193247e2e13ae032d56b158993
Bander Alfaifi <67489440+BanderAlfaifi@users.noreply.github.com>
2024-02-29 00:24:46 +0000

    All code provided in the lab document

--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -24,6 +24,32 @@ public class RangeTest {
 	public void testCentralValueShouldBeZero() { 
 	assertEquals("The central value of -1 and 1 should be 0", 
 	0, rangeObjectUnderTest.getCentralValue(), 0.000000001d); 
-	} 
+	}
+	
+	@Test
+	public void testGetLengthBothPositiveAndEqual() { 
+		Range r1 = new Range(2, 2); 
+		assertEquals("getLength: Did not return the expected output", 0.0, r1.getLength(), 0.000000001d); 
+	}
+	@Test
+	public void testGetLengthBothPositiveAndNotEqual() { 
+		Range r2 = new Range(4, 9);
+		assertEquals("getLength: Did not return the expected output", 5.0, r2.getLength(), 0.000000001d); 
+	}
+	@Test
+	public void testGetLengthBothNegativeAndEqual() { 
+		Range r3 = new Range(-99, -99); 
+		assertEquals("getLength: Did not return the expected output", 0.0, r3.getLength(), 0.000000001d); 
+	}
+	@Test
+	public void testGetLengthBothNegativeAndNotEqual() { 
+		Range r4 = new Range(-11, -4); 
+		assertEquals("getLength: Did not return the expected output", 7.0, r4.getLength(), 0.000000001d); 
+	}
+	@Test
+	public void testGetLengthOnePositiveOneNegative() { 
+		Range r5 = new Range(-5, 3); 
+		assertEquals("getLength: Did not return the expected output", 8.0, r5.getLength(), 0.000000001d); 
+	}
 
 }
